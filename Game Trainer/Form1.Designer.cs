6ee18f1065ff3bbb2dbb2dbbb1635be105abf304
Matthew Olsen <mattolse592@ed.amdsb.ca>
2023-01-24 11:20:06 -0500

    Done 2023-01-24

--- Game Trainer/Form1.Designer.cs
+++ Game Trainer/Form1.Designer.cs
@@ -33,6 +33,14 @@
             this.menuTick = new System.Windows.Forms.Timer(this.components);
             this.aimTrainerCountdown = new System.Windows.Forms.Timer(this.components);
             this.aimTrainer = new System.Windows.Forms.Timer(this.components);
+            this.retryButton = new System.Windows.Forms.Button();
+            this.menuButton = new System.Windows.Forms.Button();
+            this.numMemoryTick = new System.Windows.Forms.Timer(this.components);
+            this.numMemoryOutput = new System.Windows.Forms.Label();
+            this.numMemoryInput = new System.Windows.Forms.TextBox();
+            this.numMemorySubmit = new System.Windows.Forms.Button();
+            this.reactionTick = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // menuTick
@@ -50,12 +58,101 @@
             this.aimTrainer.Interval = 20;
             this.aimTrainer.Tick += new System.EventHandler(this.aimTrainer_Tick);
             // 
+            // retryButton
+            // 
+            this.retryButton.BackColor = System.Drawing.Color.DodgerBlue;
+            this.retryButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.retryButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.retryButton.ForeColor = System.Drawing.Color.White;
+            this.retryButton.Location = new System.Drawing.Point(230, 369);
+            this.retryButton.Name = "retryButton";
+            this.retryButton.Size = new System.Drawing.Size(60, 30);
+            this.retryButton.TabIndex = 0;
+            this.retryButton.Text = "Retry";
+            this.retryButton.UseVisualStyleBackColor = false;
+            this.retryButton.Click += new System.EventHandler(this.retryButton_Click);
+            // 
+            // menuButton
+            // 
+            this.menuButton.BackColor = System.Drawing.Color.Red;
+            this.menuButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.menuButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuButton.ForeColor = System.Drawing.Color.White;
+            this.menuButton.Location = new System.Drawing.Point(630, 369);
+            this.menuButton.Name = "menuButton";
+            this.menuButton.Size = new System.Drawing.Size(60, 30);
+            this.menuButton.TabIndex = 1;
+            this.menuButton.Text = "Menu";
+            this.menuButton.UseVisualStyleBackColor = false;
+            this.menuButton.Click += new System.EventHandler(this.menuButton_Click);
+            // 
+            // numMemoryTick
+            // 
+            this.numMemoryTick.Interval = 20;
+            this.numMemoryTick.Tick += new System.EventHandler(this.numMemoryTick_Tick);
+            // 
+            // numMemoryOutput
+            // 
+            this.numMemoryOutput.AutoSize = true;
+            this.numMemoryOutput.BackColor = System.Drawing.Color.Transparent;
+            this.numMemoryOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numMemoryOutput.ForeColor = System.Drawing.Color.White;
+            this.numMemoryOutput.Location = new System.Drawing.Point(300, 125);
+            this.numMemoryOutput.Name = "numMemoryOutput";
+            this.numMemoryOutput.Size = new System.Drawing.Size(118, 42);
+            this.numMemoryOutput.TabIndex = 2;
+            this.numMemoryOutput.Text = "label1";
+            this.numMemoryOutput.Visible = false;
+            // 
+            // numMemoryInput
+            // 
+            this.numMemoryInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numMemoryInput.Location = new System.Drawing.Point(307, 269);
+            this.numMemoryInput.Name = "numMemoryInput";
+            this.numMemoryInput.Size = new System.Drawing.Size(168, 26);
+            this.numMemoryInput.TabIndex = 3;
+            this.numMemoryInput.Visible = false;
+            // 
+            // numMemorySubmit
+            // 
+            this.numMemorySubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numMemorySubmit.Location = new System.Drawing.Point(496, 270);
+            this.numMemorySubmit.Name = "numMemorySubmit";
+            this.numMemorySubmit.Size = new System.Drawing.Size(73, 26);
+            this.numMemorySubmit.TabIndex = 4;
+            this.numMemorySubmit.Text = "Sumbit";
+            this.numMemorySubmit.UseVisualStyleBackColor = true;
+            this.numMemorySubmit.Visible = false;
+            this.numMemorySubmit.Click += new System.EventHandler(this.numMemorySubmit_Click);
+            // 
+            // reactionTick
+            // 
+            this.reactionTick.Interval = 200;
+            this.reactionTick.Tick += new System.EventHandler(this.reactionTick_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(50, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "WASD to move";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(900, 600);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numMemorySubmit);
+            this.Controls.Add(this.numMemoryInput);
+            this.Controls.Add(this.numMemoryOutput);
+            this.Controls.Add(this.menuButton);
+            this.Controls.Add(this.retryButton);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -63,12 +160,12 @@
             this.Opacity = 0.99D;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Game Trainer";
-            this.TopMost = true;
             this.Click += new System.EventHandler(this.Form1_Click);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -77,6 +174,14 @@
         private System.Windows.Forms.Timer menuTick;
         private System.Windows.Forms.Timer aimTrainerCountdown;
         private System.Windows.Forms.Timer aimTrainer;
+        private System.Windows.Forms.Button retryButton;
+        private System.Windows.Forms.Button menuButton;
+        private System.Windows.Forms.Timer numMemoryTick;
+        private System.Windows.Forms.Label numMemoryOutput;
+        private System.Windows.Forms.TextBox numMemoryInput;
+        private System.Windows.Forms.Button numMemorySubmit;
+        private System.Windows.Forms.Timer reactionTick;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Game Trainer/Form1.cs
+++ Game Trainer/Form1.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿//Matthew Olsen 2023-01-24
+//A game with three basic skill testing games called aimtrainer, reactionspeed test, and number memory test. 
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -9,28 +12,48 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
 using System.Media;
+using System.Diagnostics;
 
 namespace Game_Trainer
 {
     public partial class Form1 : Form
     {
         Random randGen = new Random();
+        Stopwatch aimTrainerStopwatch = new Stopwatch();
+        Stopwatch reactionStopwatch = new Stopwatch();
 
         Rectangle hero = new Rectangle(450, 100, 30, 30);
         Rectangle exitButton = new Rectangle(825, 15, 60, 30);
         Rectangle aimTrainerButton = new Rectangle(150, 200, 150, 100);
+        Rectangle numMemoryButton = new Rectangle(550, 200, 150, 100);
+        Rectangle reactionButton = new Rectangle(350, 200, 150, 100);
         Rectangle target;
+
+        SoundPlayer popPlayer = new SoundPlayer(Properties.Resources.pop);
 
         int heroSpeed = 10;
 
         int exitLength = 0;
         int aimTrainerLength = 0;
+        int numMemoryLength = 0;
+        int reactionLength = 0;
 
         int aimTrainerCountdownnum = 99;
-        int aimTrainerTimeLeft = 3000;
         int aimTrainerScore;
         int aimTrainerMisses;
+        double aimTrainerAccuracy;
+        int aimTrainerClicks;
         bool targetOnScreen = false;
+
+        int numMemoryScore;
+        int numMemoryShownTime;
+        int numMemory;
+        bool numMemoryCreated;
+        int numMin = 1;
+        int numMax = 10;
+
+        int reactionRound;
+        double reactionAverage;
 
         string gameState;
 
@@ -39,6 +62,7 @@ namespace Game_Trainer
         bool sDown = false;
         bool dDown = false;
 
+
         SolidBrush orangeBrush = new SolidBrush(Color.Orange);
         SolidBrush redBrush = new SolidBrush(Color.Red);
         SolidBrush whiteBrush = new SolidBrush(Color.White);
@@ -46,45 +70,102 @@ namespace Game_Trainer
         Font exitFont = new Font("Microsoft Sans Serif", 12, FontStyle.Bold);
         Font gameFont = new Font("Microsoft Sans Serif", 18, FontStyle.Bold);
         Font titleFont = new Font("Microsoft Sans Serif", 35, FontStyle.Bold);
+
         public Form1()
         {
             InitializeComponent();
             MenuSetup();
         }
+        //set up functions
         public void MenuSetup()
         {
             this.Focus();
-
+            this.BackColor = Color.Black;
             hero.X = 450;
             hero.Y = 100;
+            label1.Visible = true;
 
             menuTick.Enabled = true;
 
+            menuButton.Visible = false;
+            retryButton.Visible = false;
+
+            aimTrainerLength = 0;
+            numMemoryLength = 0;
+            reactionLength = 0;
+            numMemory = 0;
+            numMemoryInput.Visible = false;
+            numMemoryOutput.Visible = false;
+            numMemorySubmit.Visible = false;
+
             gameState = "menu";
+
+            this.Focus();
         }
         public void AimTrainerCountdownSetup()
         {
             menuTick.Enabled = false;
             aimTrainerCountdown.Enabled = true;
+            aimTrainerCountdownnum = 99;
+
+            menuButton.Visible = false;
+            retryButton.Visible = false;
 
             gameState = "aimTrainerCountdown";
 
             Refresh();
         }
-
         public void AimTrainerSetup()
         {
+            aimTrainerStopwatch.Reset();
             aimTrainerCountdown.Enabled = false;
             aimTrainer.Enabled = true;
             targetOnScreen = false;
             aimTrainerScore = 0;
             aimTrainerMisses = 0;
+            aimTrainerAccuracy = 0;
+            aimTrainerClicks = 0;
 
-            aimTrainerTimeLeft = 3000;
+            aimTrainerStopwatch.Start();
 
             gameState = "aimTrainer";
         }
+        public void NumberMemorySetup()
+        {
+            menuTick.Enabled = false;
+            numMemoryTick.Enabled = true;
+            numMemoryOutput.Visible = true;
+            numMemoryCreated = false;
+            menuButton.Visible = false;
+            retryButton.Visible = false;
+            numMemoryInput.Visible = false;
+            numMemorySubmit.Visible = false;
+            numMemoryInput.Text = "";
 
+            numMin = 1;
+            numMax = 10;
+
+            numMemoryScore = 0;
+            numMemoryShownTime = 900;
+
+            gameState = "numMemoryShow";
+        }
+        public void ReactionSetup()
+        {
+            menuTick.Enabled = false;
+            reactionTick.Enabled = true;
+            reactionRound = 0;
+            this.BackColor = Color.Red;
+            reactionStopwatch.Reset();
+            menuButton.Visible = false;
+            retryButton.Visible = false;
+            numMemoryOutput.Visible = false;
+            reactionAverage = 0;
+
+            reactionTick.Interval = randGen.Next(500, 2001);
+
+            gameState = "reactionSpeed";
+        }
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
             switch (e.KeyCode)
@@ -102,6 +183,7 @@ namespace Game_Trainer
                     dDown = true;
                     break;
             }
+            label1.Visible = false;
         }
         private void Form1_KeyUp(object sender, KeyEventArgs e)
         {
@@ -121,23 +203,75 @@ namespace Game_Trainer
                     break;
             }
         }
-
         private void Form1_Click(object sender, EventArgs e)
         {
-            int x = Cursor.Position.X - this.Location.X;
-            int y = Cursor.Position.Y - this.Location.Y;
+            if (gameState == "aimTrainer")
+            {
 
-            if (x > target.X && x < target.X + target.Width && y > target.Y && y < target.Y + target.Height)
-            {
-                targetOnScreen = false;
-                aimTrainerScore++;
+                int x = Cursor.Position.X - this.Location.X;
+                int y = Cursor.Position.Y - this.Location.Y;
+
+                if (x > target.X && x < target.X + target.Width && y > target.Y && y < target.Y + target.Height)
+                {
+                    targetOnScreen = false;
+                    popPlayer.Play();
+                    aimTrainerScore++;
+                }
+                else
+                {
+                    aimTrainerMisses++;
+                }
+
+                aimTrainerClicks = aimTrainerMisses + aimTrainerScore;
+                aimTrainerAccuracy = (double)aimTrainerScore / (double)aimTrainerClicks;
             }
-            else
+            else if (gameState == "reactionSpeed")
             {
-                aimTrainerMisses++;
+                if (this.BackColor == Color.Red)
+                {
+                    gameState = "reactionSpeedEarly";
+                    numMemoryOutput.Visible = true;
+                    this.BackColor = Color.Black;
+                    reactionTick.Enabled = false;
+                    Refresh();
+                }
+                else if (this.BackColor == Color.Green)
+                {
+                    gameState = "reactionSpeedGood";
+                    this.BackColor = Color.Black;
+                    numMemoryOutput.Visible = true;
+                    reactionStopwatch.Stop();
+                    reactionRound++;
+                    reactionAverage += Convert.ToDouble(reactionStopwatch.ElapsedMilliseconds);
+                    
+                    Refresh();
+                }
+
+            }
+            else if (gameState == "reactionSpeedEarly" || gameState == "reactionSpeedGood")
+            {
+                if (reactionRound == 5)
+                {
+                    gameState = "reactionSpeedEnd";
+                    reactionAverage /= 5;
+
+                    menuButton.Visible = true;
+                    retryButton.Visible = true;
+                    Refresh();
+                }
+                else
+                {
+                    this.BackColor = Color.Red;
+                    numMemoryOutput.Visible = false;
+                    gameState = "reactionSpeed";
+                    reactionTick.Interval = randGen.Next(500, 2001);
+                    reactionTick.Enabled = true;
+
+                    reactionStopwatch.Reset();
+                }
             }
         }
-
+        //game tick events
         private void gameTick_Tick(object sender, EventArgs e)
         {
             if (wDown == true && hero.Y > 0)
@@ -183,6 +317,32 @@ namespace Game_Trainer
                 AimTrainerCountdownSetup();
             }
 
+            if (hero.IntersectsWith(numMemoryButton))
+            {
+                numMemoryLength += 3;
+            }
+            else if (numMemoryLength > 0)
+            {
+                numMemoryLength -= 5;
+            }
+            if (numMemoryLength >= 151)
+            {
+                NumberMemorySetup();
+            }
+
+            if (hero.IntersectsWith(reactionButton))
+            {
+                reactionLength += 3;
+            }
+            else if (reactionLength > 0)
+            {
+                reactionLength -= 5;
+            }
+            if (reactionLength >= 151)
+            {
+                ReactionSetup();
+            }
+
             Refresh();
 
         }
@@ -206,19 +366,51 @@ namespace Game_Trainer
                 targetOnScreen = true;
             }
 
-            if (aimTrainerTimeLeft > 0)
-            {
-                aimTrainerTimeLeft -= 3;
-            }
-            else
+            if (aimTrainerStopwatch.ElapsedMilliseconds > 30000)
             {
                 gameState = "aimTrainerOver";
-            }
 
+                aimTrainer.Enabled = false;
+
+                menuButton.Visible = true;
+                retryButton.Visible = true;
+            }
 
             Refresh();
         }
+        private void numMemoryTick_Tick(object sender, EventArgs e)
+        {
+            if (numMemoryShownTime < 0)
+            {
+                gameState = "numMemoryInput";
 
+                numMemoryTick.Enabled = false;
+                numMemoryOutput.Visible = false;
+                numMemoryInput.Visible = true;
+                numMemorySubmit.Visible = true;
+
+            }
+            else
+            {
+                if (numMemoryCreated == false)
+                {
+                    numMemory = randGen.Next(numMin, numMax);
+                    numMemoryOutput.Text = "";
+                    numMemoryCreated = true;
+                }
+
+                numMemoryShownTime -= 7;
+                Refresh();
+            }
+
+        }
+        private void reactionTick_Tick(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Green;
+            reactionStopwatch.Start();
+            reactionTick.Interval = randGen.Next(500, 2001);
+            reactionTick.Enabled = false;
+        }
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             if (gameState == "menu")
@@ -227,11 +419,29 @@ namespace Game_Trainer
                 e.Graphics.FillRectangle(whiteBrush, 825, 45, exitLength, 3);
                 e.Graphics.DrawRectangle(whitePen, aimTrainerButton);
                 e.Graphics.FillRectangle(whiteBrush, 150, 300, aimTrainerLength, 3);
+                e.Graphics.DrawRectangle(whitePen, numMemoryButton);
+                e.Graphics.FillRectangle(whiteBrush, numMemoryButton.X, 300, numMemoryLength, 3);
+                e.Graphics.DrawRectangle(whitePen, reactionButton);
+                e.Graphics.FillRectangle(whiteBrush, reactionButton.X, 300, reactionLength, 3);
 
                 e.Graphics.DrawString("Exit", exitFont, whiteBrush, 835, 20);
                 e.Graphics.DrawString("Aim Trainer", gameFont, whiteBrush, aimTrainerButton.X + 5, aimTrainerButton.Y + 37);
                 e.Graphics.DrawString("Game Trainer", titleFont, whiteBrush, 280, 20);
+                e.Graphics.DrawString("Number\nMemory\nTest", gameFont, whiteBrush, numMemoryButton.X + 25, numMemoryButton.Y + 10);
+                e.Graphics.DrawString("Reaction \nSpeed", gameFont, whiteBrush, reactionButton.X + 15, reactionButton.Y + 20);
 
+                if (aimTrainerLength > 0)
+                {
+                    e.Graphics.DrawString("How many targets can \nyou hit in 30 seconds?", exitFont, whiteBrush, aimTrainerButton.X, aimTrainerButton.Y + 110);
+                }
+                if (numMemoryLength > 0)
+                {
+                    e.Graphics.DrawString("What is the longest number\n you can remember?", exitFont, whiteBrush, numMemoryButton.X, numMemoryButton.Y + 110);
+                }
+                if (reactionLength > 0)
+                {
+                    e.Graphics.DrawString("Test your visual reflexes!\nClick when the screen turns green.", exitFont, whiteBrush, reactionButton.X, reactionButton.Y + 110);
+                }
                 e.Graphics.FillRectangle(orangeBrush, hero);
             }
             else if (gameState == "aimTrainerCountdown")
@@ -251,15 +461,87 @@ namespace Game_Trainer
             }
             else if (gameState == "aimTrainer")
             {
-                e.Graphics.DrawString(Convert.ToString(aimTrainerTimeLeft), gameFont, whiteBrush, 450, 20);
+                e.Graphics.DrawString($"{(aimTrainerStopwatch).Elapsed.ToString(@"ss\:ff")}", gameFont, whiteBrush, 450, 20);
                 e.Graphics.FillEllipse(redBrush, target);
+                e.Graphics.DrawString($"Score: {aimTrainerScore}\nMisses: {aimTrainerMisses}\nAccuracy: {aimTrainerAccuracy.ToString("P")}", gameFont, whiteBrush, 30, 20);
+
             }
             else if (gameState == "aimTrainerOver")
             {
                 e.Graphics.DrawString("Game Over", titleFont, whiteBrush, 300, 250);
+                e.Graphics.DrawString($"Score: {aimTrainerScore}\nMisses: {aimTrainerMisses}\nAccuracy: {aimTrainerAccuracy.ToString("P")}", gameFont, whiteBrush, 300, 300);
+            }
+            else if (gameState == "numMemoryShow")
+            {
+                e.Graphics.DrawString($"Score: {numMemoryScore}", titleFont, whiteBrush, 280, 20);
+                e.Graphics.FillRectangle(whiteBrush, 0, 597, numMemoryShownTime, 3);
+
+                if (numMemoryCreated == true)
+                {
+                    numMemoryOutput.Text = $"{Convert.ToString(numMemory)}";
+                }
+            }
+            else if (gameState == "reactionSpeedEarly")
+            {
+                numMemoryOutput.Text = "Too soon. \nClick to try again.";
+            }
+            else if (gameState == "reactionSpeedGood")
+            {
+                numMemoryOutput.Text = reactionStopwatch.Elapsed.ToString(@"ss\:fff");
+                numMemoryOutput.Text += "\n Click to try again.";
+            }
+            else if (gameState == "reactionSpeedEnd")
+            {
+                numMemoryOutput.Text = $"Average time: {reactionAverage}ms";
             }
         }
+        private void retryButton_Click(object sender, EventArgs e)
+        {
+            if (gameState == "aimTrainerOver")
+            {
+                AimTrainerCountdownSetup();
+            }
+            if (gameState == "numMemoryInput")
+            {
+                NumberMemorySetup();
+            }
+            if (gameState == "reactionSpeedEnd")
+            {
+                ReactionSetup();
+            }
+        }
+        private void menuButton_Click(object sender, EventArgs e)
+        {
+            MenuSetup();
+        }
+        private void numMemorySubmit_Click(object sender, EventArgs e)
+        {
+            numMemoryOutput.Visible = true;
+            if (numMemoryInput.Text == Convert.ToString(numMemory))
+            {
+                numMemoryOutput.Text = $"Correct. \nThe correct number was \n{numMemory}";
+                numMemoryScore++;
+                Refresh();
+                Thread.Sleep(1000);
+                gameState = "numMemoryShow";
 
+                numMemoryTick.Enabled = true;
+                numMemoryOutput.Visible = true;
+                numMemoryInput.Visible = false;
+                numMemorySubmit.Visible = false;
+                numMemoryCreated = false;
 
+                numMin *= 10;
+                numMax *= 10;
+                numMemoryShownTime = 900;
+                numMemoryInput.Text = "";
+            }
+            else
+            {
+                numMemoryOutput.Text = $"Incorrect. \nThe correct number was\n {numMemory}";
+                retryButton.Visible = true;
+                menuButton.Visible = true;
+            }
+        }
     }
 }
